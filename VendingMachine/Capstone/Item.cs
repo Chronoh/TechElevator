7a1676c0801c537ad95da98a0beade323477c82d
Josiah Kuha <jonkaha@gmail.com>
2020-07-12 23:31:23 -0400

    Adding Vending Machine project from Tech Elevator

---
+++ VendingMachine/Capstone/Item.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Capstone
+{
+    public class Item
+    {
+        public Item(string itemInfo)
+        {
+            SetupItemInfo(itemInfo);
+            Quantity = "Quant: 5";
+        }
+        
+       
+        public string SlotLocation { get; private set; }
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+        public string Type { get; private set; } // consider subclass based on type w/ abstract use of item class
+        public string Quantity { get; private set; }  //setting type to string so that Quantity == 0 --> string "SOLD OUT"
+        //public string Quantity { get; private set; }  //use the string prop to be used as the 'displayed' qty. use an INT prop for the actual qty.
+
+        public void SetupItemInfo(string itemInfo)
+        {
+            string[] dataArray = itemInfo.Split("|");
+            SlotLocation = dataArray[0];
+            Name = dataArray[1];
+            Price = decimal.Parse(dataArray[2]);
+            Type = dataArray[3];
+        }
+
+        public void DecrementItemQuantity()
+        {   
+            try
+            {
+                int quantityAsInt = int.Parse(Quantity.Substring(7));
+                if (quantityAsInt > 0) 
+                {
+                    quantityAsInt--;
+                    Quantity = "Quant: " + quantityAsInt.ToString();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Item is sold out!"); //Error message
+                
+            }
+            finally
+            {
+                if (Quantity.Contains("0"))
+                {
+                    Quantity = "SOLD OUT";
+                }
+            }
+        }
+    }
+}
+
